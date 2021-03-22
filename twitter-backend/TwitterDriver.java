1b0bf5151991fe137ab6ea2bb4bc9f67ed456e41
Rashel Rojas <rdr2139@columbia.edu>
2021-03-22 15:03:16 -0400

    Backend: fixed error by removing function that made tweets

--- twitter-backend/TwitterDriver.java
+++ twitter-backend/TwitterDriver.java
@@ -21,32 +21,19 @@ public class TwitterDriver
       // set up classpath and properties file
       Twitterer bigBird = new Twitterer(consolePrint);
 
-      // Testing the API: Make a tweet via tweetOut()
-      String message = "I'm testing out the twitter4j API for Java.  Thanks @cscheerleader! "
-                     + "You can find out more at https://github.com/riagalanos/cs1-twitter";
-      bigBird.tweetOut(message);
-
-
-      // ** User enters a hashtag movement **
+      // User enters a hashtag movement 
       Scanner scan = new Scanner(System.in);
-      System.out.print("Please enter a hashtag movement, including the '#' symbol.");
+      System.out.print("Please enter a hashtag movement, excluding the '#' symbol.");
       String twitter_hashtag = scan.next();
-
-      // returns tweets containing the hashtag as a list/json
-      bigBird.saQuery(twitter_hashtag);
-
+      bigBird.saQuery("#" + twitter_hashtag);
+      scan.close();      
 
       /** Next steps:
        * in twitter.java --- make saQuery() return a list of the tweets
-       * search for tweets that contain 'donate', 'petition', 'volunteer' in .getText()
-       *    if tweet contains a nonempty url, getURL() --> add to a list
-       * sort by most likes/retweets
+       * (extra) sort by most likes/retweets
        * convert list to json?
        * send json to frontend
       */
-
-
-      scan.close();      
 
    }         
       
--- twitter-backend/Twitterer.java
+++ twitter-backend/Twitterer.java
@@ -1,7 +1,4 @@
-import twitter4j.GeoLocation;       // jar found at http://twitter4j.org/en/index.html
-import twitter4j.Paging;
 import twitter4j.Query;
-//import twitter4j.FilterQuery;
 import twitter4j.QueryResult;
 import twitter4j.Status;
 import twitter4j.Twitter;
@@ -29,36 +26,12 @@ public class Twitterer
          twitter = TwitterFactory.getSingleton(); 
          consolePrint = console;
          statuses = new ArrayList<Status>();
-      }
-   
-      // This method tweets a given message.
-      // @param String  a message you wish to Tweet out
-
-      public void tweetOut(String message) throws TwitterException, IOException
-      {
-         Status status = twitter.updateStatus(message);
-         System.out.println("Successfully updated the status to [" + status.getText() + "].");
-      }  
-    
+      }    
 
       // This method finds the last 100 queries of tweets since 2016 with the hashtag
 
       public void saQuery (String hashtag) throws TwitterException
       {
-
-         /*
-         // Create a FilterQuery object and then set the items to track
-         FilterQuery filterQuery = new FilterQuery();
-         
-         // Create an array of items to track
-         String[] itemsToTrack = {hashtag, "donate"};
-         
-         // Set the items to track using FilterQuerys' track method.
-         filterQuery.track(itemsToTrack);
-
-         // Assuming you have already created Twitter/TwitterStream object, use the filter method to start streaming using the FilterQuery object just created.
-         twitterstream.filter(filterQuery);
-         */
 
          Query query = new Query(hashtag);
          query.setCount(100);                
@@ -69,15 +42,6 @@ public class Twitterer
             System.out.println("Count : " + result.getTweets().size()) ;
             for (Status tweet : result.getTweets()) {
                System.out.println("@"+tweet.getUser().getName()+ ": " + tweet.getText());  
-
-               // print links if tweet contains keywords
-               if (tweetContainsKeywords(tweet.getText())) {
-                  // prints URLs in tweet (later add them to a list)
-                  if (tweet.getURLEntities().length != 0) {
-                     for (URLEntity urle : tweet.getURLEntities())     
-                        System.out.println(urle.getDisplayURL());  
-                  }
-               }
 
             }
          } 
