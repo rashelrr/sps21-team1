c01b50a00f650b766aed7e6116f6f2310d136a98
Rashel Rojas <rdr2139@columbia.edu>
2021-03-29 14:40:11 -0400

    Backend: added Index and script files

---
+++ src/main/webapp/script.js
@@ -0,0 +1,37 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * Adds a random greeting to the page.
+ */
+ function addRandomGreeting() {
+    const greetings =
+        ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
+  
+    // Pick a random greeting.
+    const greeting = greetings[Math.floor(Math.random() * greetings.length)];
+  
+    // Add it to the page.
+    const greetingContainer = document.getElementById('greeting-container');
+    greetingContainer.innerText = greeting;
+}
+async function getBot() {
+    const responseFromServer = await fetch('/twitter-bot?' + new URLSearchParams({
+    hashtag: '#stopasianhate',
+    }));
+    const textFromResponse = await responseFromServer.text();
+
+    const botContainer = document.getElementById('bot');
+    botContainer.innerText = textFromResponse;
+}
