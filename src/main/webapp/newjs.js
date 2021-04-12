ed611123a71f6f2af30532d8cdf4d3f3ac8c6a95
CParker-dotcom <chanteparker100@gmail.com>
2021-04-12 14:22:56 +0000

    Change the newindex.html and the newjs.js files

--- src/main/webapp/newjs.js
+++ src/main/webapp/newjs.js
@@ -1,10 +1,22 @@
-const list = document.getElementById('movement');
-
-function createMovementElement() {
-   const movementname = document.getElementById('movementname').value;
-    document.getElementById('movementid').innerHTML = movementname;
-    const movementElement = document.createElement('li');
-    movementElement.appendChild(document.createTextNode(movementname));
-    list.appendChild(movementElement);
+/** Fetches movement hashtags from the server and adds them to the DOM. */
+function loadMovements() {
+  fetch('/get-hashtag').then(response => response.json()).then((moevents) => {
+    const movementListElement = document.getElementById('movement-list');
+    movements.forEach((movement) => {
+      movementListElement.appendChild(createMovementElement(movement));
+    })
+  });
 }
 
+/** Creates an element that represents a task, including its delete button. */
+function createMovementElement(movement) {
+  const movementElement = document.createElement('li');
+
+  const titleElement = document.createElement('span');
+  titleElement.innerText = movement.title;
+
+/**Loads movements automatically */
+  window.onload = (movement) => {
+  console.log('page is fully loaded');
+  }
+}
