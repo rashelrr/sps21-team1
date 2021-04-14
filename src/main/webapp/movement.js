9a5afc783cefca562727bc886d6f7c61acb770fa
CParker-dotcom <chanteparker100@gmail.com>
2021-04-14 12:19:20 +0000

    Change some functions, add some functions and still have some questions

--- src/main/webapp/movement.js
+++ src/main/webapp/movement.js
@@ -7,34 +7,38 @@ function callsotherfunctions(){
 
 /** Fetches movement hashtags from the server and adds them to the DOM. */
 function loadgetmovement() {
-  fetch('/get-movement').then(response => response.json()).then((movements) => {
-    const movementListElement = document.getElementById('movement-list');
-    movements.forEach((movement) => {
-      movementListElement.appendChild(createMovementElement(movement));
-    })
-  });
+  fetch('/get-movement').then(response => response.json()).then((movements));
+  const movementListElement = document.getElementById('movement-list');
+  movementListElement.appendChild(getMovementElement(movement));
+    
+  }
+
+
+/** Uses the fields that were in Stop Asian Hate */
+function getmovementElement(movement) {
+    getmovementElement.name = 'name';
+    getmovementElement.description = 'description';
+    getmovementElement.hashtag = 'hashtag';
+    getmovementElement.video = 'video';
+    getmovementElement.image = 'image';
+    getmovementElement.donatation = 'donatation';
+    getmovementElement.resources = 'resources'; 
 }
 
 
-function creategetmovementElement(movement) {
-  const getmovementElement = document.createElement('li');
-  getmovementElement.className = 'movement';
-
-  const nameElement = document.createElement('span');
-  nameElement.innerText = movement.name;
-
-  const descriptionElement = document.createElement('span');
-  descriptionElement.innerText = movement.description;
-
-  const hashtagElement = document.createElement('span');
-  hashtagElement.innerText = movement.hashtag;
-
-  taskElement.appendChild(titleElement);
-  taskElement.appendChild(deleteButtonElement);
-  return taskElement;
-}
 
 
 for (var i = 0, keys = Object.keys(LinkedHashMap), ii = keys.length; i < ii; i++) {
   console.log('key : ' + keys[i] + ' val : ' + LinkedHashMap[keys[i]]);
+}
+
+async function getBot() {
+    const responseFromServer = await fetch('/twitter-bot' + new URLSearchParams({
+    hashtag: '#stopasianhate',
+
+  }));
+  const textFromResponse = await responseFromServer.text();
+
+  const botContainer = document.getElementById('bot');
+  botContainer.innerText = textFromResponse;
 }
